16814799f0636bd514e34de3c36fe8945910bcd7
CVXIV <898634520@qq.com>
2020-08-23 22:48:50 +0800

    游戏结束判定和细节优化

--- Assets/Scripts/2048/Board.cs
+++ Assets/Scripts/2048/Board.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Board {
+    private delegate bool Move();
+
     private const int cols = 4;
     private const int rows = 4;
     private readonly int[,] board;
+    private readonly int[,] tempBoard;
     private readonly List<Location> remainLocation;
+    private readonly Dictionary<Direction, Move> dirToMove;
+
 
     public Board() {
         board = new int[rows, cols];
+        tempBoard = new int[rows, cols];
         remainLocation = new List<Location>(cols * rows);
+
+        dirToMove = new Dictionary<Direction, Move> {
+            { Direction.Up, new Move(this.MoveUp) },
+            { Direction.Down, new Move(this.MoveDown) },
+            { Direction.Left, new Move(this.MoveLeft) },
+            { Direction.Right, new Move(this.MoveRight) }
+        };
     }
 
     /// <summary>
@@ -31,116 +44,166 @@ public class Board {
         }
     }
 
+    private bool MoveUp() {
+        System.Array.Clear(tempBoard, 0, tempBoard.Length);
+        for (int i = 0; i < cols; ++i) {
+            int index = 0;
+            for (int j = 0; j < rows; ++j) {
+                if (board[j, i] != 0) {
+                    tempBoard[index++, i] = board[j, i];
+                }
+            }
+
+            for (int mergeIndex = 0; mergeIndex < rows - 1; ++mergeIndex) {
+                if (tempBoard[mergeIndex, i] == 0) {
+                    break;
+                }
+                if (tempBoard[mergeIndex, i] == tempBoard[mergeIndex + 1, i]) {
+                    tempBoard[mergeIndex, i] += tempBoard[mergeIndex + 1, i];
+                    tempBoard[mergeIndex + 1, i] = 0;
+                    for (int j = mergeIndex + 1; j < rows - 1; ++j) {
+                        tempBoard[j, i] = tempBoard[j + 1, i];
+                        tempBoard[j + 1, i] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+        return this.CheckBoard();
+    }
+
+    private bool MoveDown() {
+        System.Array.Clear(tempBoard, 0, tempBoard.Length);
+        for (int i = 0; i < cols; ++i) {
+            int index = rows - 1;
+            for (int j = rows - 1; j >= 0; --j) {
+                if (board[j, i] != 0) {
+                    tempBoard[index--, i] = board[j, i];
+                }
+            }
+
+            for (int mergeIndex = rows - 1; mergeIndex > 0; --mergeIndex) {
+                if (tempBoard[mergeIndex, i] == 0) {
+                    break;
+                }
+                if (tempBoard[mergeIndex, i] == tempBoard[mergeIndex - 1, i]) {
+                    tempBoard[mergeIndex, i] += tempBoard[mergeIndex - 1, i];
+                    tempBoard[mergeIndex - 1, i] = 0;
+                    for (int j = mergeIndex - 1; j > 0; --j) {
+                        tempBoard[j, i] = tempBoard[j - 1, i];
+                        tempBoard[j - 1, i] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+        return this.CheckBoard();
+    }
+
+    private bool MoveLeft() {
+        System.Array.Clear(tempBoard, 0, tempBoard.Length);
+        for (int i = 0; i < rows; ++i) {
+            int index = 0;
+            for (int j = 0; j < cols; ++j) {
+                if (board[i, j] != 0) {
+                    tempBoard[i, index++] = board[i, j];
+                }
+            }
+
+            for (int mergeIndex = 0; mergeIndex < cols - 1; ++mergeIndex) {
+                if (tempBoard[i, mergeIndex] == 0) {
+                    break;
+                }
+                if (tempBoard[i, mergeIndex] == tempBoard[i, mergeIndex + 1]) {
+                    tempBoard[i, mergeIndex] += tempBoard[i, mergeIndex + 1];
+                    tempBoard[i, mergeIndex + 1] = 0;
+                    for (int j = mergeIndex + 1; j < cols - 1; ++j) {
+                        tempBoard[i, j] = tempBoard[i, j + 1];
+                        tempBoard[i, j + 1] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+        return this.CheckBoard();
+    }
+
+    private bool MoveRight() {
+        System.Array.Clear(tempBoard, 0, tempBoard.Length);
+        for (int i = 0; i < rows; ++i) {
+            int index = cols - 1;
+            for (int j = cols - 1; j >= 0; --j) {
+                if (board[i, j] != 0) {
+                    tempBoard[i, index--] = board[i, j];
+                }
+            }
+
+            for (int mergeIndex = cols - 1; mergeIndex > 0; --mergeIndex) {
+                if (tempBoard[i, mergeIndex] == 0) {
+                    break;
+                }
+                if (tempBoard[i, mergeIndex] == tempBoard[i, mergeIndex - 1]) {
+                    tempBoard[i, mergeIndex] += tempBoard[i, mergeIndex - 1];
+                    tempBoard[i, mergeIndex - 1] = 0;
+                    for (int j = mergeIndex - 1; j > 0; --j) {
+                        tempBoard[i, j] = tempBoard[i, j - 1];
+                        tempBoard[i, j - 1] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+        return this.CheckBoard();
+    }
+
     /// <summary>
     /// 上下左右移动格子
     /// </summary>
     public void ResortMerge(Direction dir) {
-        int[,] temp = new int[rows, cols];
-        switch (dir) {
-            case Direction.Up:
-                for (int i = 0; i < cols; ++i) {
-                    int index = 0;
-                    for (int j = 0; j < rows; ++j) {
-                        if (board[j, i] != 0) {
-                            temp[index++, i] = board[j, i];
-                        }
-                    }
-
-                    for (int mergeIndex = 0; mergeIndex < rows - 1; ++mergeIndex) {
-                        if (temp[mergeIndex, i] == 0) {
-                            break;
-                        }
-                        if (temp[mergeIndex, i] == temp[mergeIndex + 1, i]) {
-                            temp[mergeIndex, i] += temp[mergeIndex + 1, i];
-                            temp[mergeIndex + 1, i] = 0;
-                            for (int j = mergeIndex + 1; j < rows - 1; ++j) {
-                                temp[j, i] = temp[j + 1, i];
-                                temp[j + 1, i] = 0;
-                            }
-                            break;
-                        }
-                    }
-                }
-                break;
-            case Direction.Down:
-                for (int i = 0; i < cols; ++i) {
-                    int index = rows - 1;
-                    for (int j = rows - 1; j >= 0; --j) {
-                        if (board[j, i] != 0) {
-                            temp[index--, i] = board[j, i];
-                        }
-                    }
-
-                    for (int mergeIndex = rows - 1; mergeIndex > 0; --mergeIndex) {
-                        if (temp[mergeIndex, i] == 0) {
-                            break;
-                        }
-                        if (temp[mergeIndex, i] == temp[mergeIndex - 1, i]) {
-                            temp[mergeIndex, i] += temp[mergeIndex - 1, i];
-                            temp[mergeIndex - 1, i] = 0;
-                            for (int j = mergeIndex - 1; j > 0; --j) {
-                                temp[j, i] = temp[j - 1, i];
-                                temp[j - 1, i] = 0;
-                            }
-                            break;
-                        }
-                    }
-                }
-                break;
-            case Direction.Left:
-                for (int i = 0; i < rows; ++i) {
-                    int index = 0;
-                    for (int j = 0; j < cols; ++j) {
-                        if (board[i, j] != 0) {
-                            temp[i, index++] = board[i, j];
-                        }
-                    }
-
-                    for (int mergeIndex = 0; mergeIndex < cols - 1; ++mergeIndex) {
-                        if (temp[i, mergeIndex] == 0) {
-                            break;
-                        }
-                        if (temp[i, mergeIndex] == temp[i, mergeIndex + 1]) {
-                            temp[i, mergeIndex] += temp[i, mergeIndex + 1];
-                            temp[i, mergeIndex + 1] = 0;
-                            for (int j = mergeIndex + 1; j < cols - 1; ++j) {
-                                temp[i, j] = temp[i, j + 1];
-                                temp[i, j + 1] = 0;
-                            }
-                            break;
-                        }
-                    }
-                }
-                break;
-            case Direction.Right:
-                for (int i = 0; i < rows; ++i) {
-                    int index = cols - 1;
-                    for (int j = cols - 1; j >= 0; --j) {
-                        if (board[i, j] != 0) {
-                            temp[i, index--] = board[i, j];
-                        }
-                    }
-
-                    for (int mergeIndex = cols - 1; mergeIndex > 0; --mergeIndex) {
-                        if (temp[i, mergeIndex] == 0) {
-                            break;
-                        }
-                        if (temp[i, mergeIndex] == temp[i, mergeIndex - 1]) {
-                            temp[i, mergeIndex] += temp[i, mergeIndex - 1];
-                            temp[i, mergeIndex - 1] = 0;
-                            for (int j = mergeIndex - 1; j > 0; --j) {
-                                temp[i, j] = temp[i, j - 1];
-                                temp[i, j - 1] = 0;
-                            }
-                            break;
-                        }
-                    }
-                }
-                break;
+        bool isMove = this.dirToMove[dir].Invoke();
+        // 如果发生了移动，则进行数据拷贝以及产生新数值
+        if (isMove) {
+            System.Array.Copy(tempBoard, board, board.Length);
+            this.BoardGenNum();
         }
-        System.Array.Copy(temp, this.board, this.board.Length);
-        /// todo如果棋盘没有变动，则不用生成新数字
-        this.BoardGenNum();
+        if (!CheckContinue()) {
+            Debug.Log("游戏结束");
+        }
+    }
+
+    /// <summary>
+    /// 检测游戏是否可以继续
+    /// </summary>
+    /// <returns></returns>
+    private bool CheckContinue() {
+        // 如果棋盘中还存在空的格子，则可以继续
+        for (int i = 0; i < cols * rows; ++i) {
+            if (board[i / cols, i % cols] == 0) {
+                return true;
+            }
+        }
+        // 如果棋盘已经满了，则需要判断是否可以移动
+        bool result = false;
+        foreach (Direction dir in System.Enum.GetValues(typeof(Direction))) {
+            result |= dirToMove[dir].Invoke();
+            if (result) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// 检测是否进行了有效的移动
+    /// </summary>
+    /// <returns></returns>
+    private bool CheckBoard() {
+        for (int i = 0; i < tempBoard.Length; ++i) {
+            if (tempBoard[i / cols, i % cols] != this.board[i / cols, i % cols]) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
---
+++ Assets/Scripts/2048/UI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+    public GameObject quitCanvas;
+    public GameObject startCanvas;
+
+    private void Start() {
+        quitCanvas.SetActive(false);
+    }
+
+    public void ExitPress() {
+        quitCanvas.SetActive(true);
+        startCanvas.SetActive(false);
+    }
+
+    public void NoPress() {
+        quitCanvas.SetActive(false);
+        startCanvas.SetActive(true);
+    }
+
+
+    public void YesPress() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    public void StartPress() {
+        gameObject.SetActive(false);
+        quitCanvas.SetActive(false);
+    }
+}
